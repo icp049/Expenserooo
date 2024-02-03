9ab4df6b72bb46df0b8a2f04e4ea96a51f5be9d4
icp049 <ianpepe2000@gmail.com>
2024-02-02 20:40:47 -0600

    Initial Commit

---
+++ Expenserooo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Expenserooo
+//
+//  Created by Ian Pedeglorio on 2024-02-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expenserooo/ExpenseroooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseroooApp.swift
+//  Expenserooo
+//
+//  Created by Ian Pedeglorio on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseroooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
