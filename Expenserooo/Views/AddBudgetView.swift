2f9ce5b033fec8970cbe9ac1344e0ca323163ce6
icp049 <ianpepe2000@gmail.com>
2024-02-03 19:19:32 -0600

    added picker in categories and rewrote format of addBudgetView, removed form enclosure and just used vstack

--- Expenserooo/Views/AddBudgetView.swift
+++ Expenserooo/Views/AddBudgetView.swift
@@ -10,47 +10,46 @@ struct AddBudgetView: View {
     @State private var expenseCategory = ""
     @State private var expenseAmount = ""
     
-
+    let categorySelection = ["Bills","Leisure", "Food", "Travel"]
+    
     @State private var expenses: [Expense] = []
 
     var body: some View {
-        Form {
-            Section {
-                TextField("Income Source", text: $name)
-                    .autocapitalization(.none)
-                    .autocorrectionDisabled()
+        VStack {
+            TextField("Income Source", text: $name)
+                .autocapitalization(.none)
+                .autocorrectionDisabled()
 
-                TextField("Amount", text: $amount)
-                    .autocapitalization(.none)
-                    .autocorrectionDisabled()
+            TextField("Amount", text: $amount)
+                .autocapitalization(.none)
+                .autocorrectionDisabled()
 
-                HStack {
-                    TextField("Expense Name", text: $expenseName)
-                        .autocapitalization(.none)
-                        .autocorrectionDisabled()
-                    
-                    TextField("Expense Category", text: $expenseCategory)
-                        .autocapitalization(.none)
-                        .autocorrectionDisabled()
+            TextField("Expense Name", text: $expenseName)
+                .autocapitalization(.none)
+                .autocorrectionDisabled()
 
-                    TextField("Expense Amount", text: $expenseAmount)
-                        .autocapitalization(.none)
-                        .autocorrectionDisabled()
-
-                    Button("Add Expense") {
-                        let newExpense = Expense(context: managedObjContext)
-                        newExpense.name = expenseName
-                        newExpense.category = expenseCategory
-                        newExpense.amount = Double(expenseAmount) ?? 0.0
-                    
-
-                        expenses.append(newExpense)
-                        // Optionally clear the text fields after adding an expense
-                        expenseName = ""
-                        expenseCategory = ""
-                        expenseAmount = ""
-                    }
+            Picker("Expense Category", selection: $expenseCategory) {
+                ForEach(categorySelection, id: \.self) { category in
+                    Text(category)
                 }
+            }
+            .pickerStyle(MenuPickerStyle())
+
+            TextField("Expense Amount", text: $expenseAmount)
+                .autocapitalization(.none)
+                .autocorrectionDisabled()
+
+            Button("Add Expense") {
+                let newExpense = Expense(context: managedObjContext)
+                newExpense.name = expenseName
+                newExpense.category = expenseCategory
+                newExpense.amount = Double(expenseAmount) ?? 0.0
+
+                expenses.append(newExpense)
+                // Optionally clear the text fields after adding an expense
+                expenseName = ""
+                expenseCategory = ""
+                expenseAmount = ""
             }
 
             Section(header: Text("Expenses")) {
@@ -62,34 +61,19 @@ struct AddBudgetView: View {
                         Spacer()
                         Text("\(expense.amount)")
                         Spacer()
-                 
-                        
                     }
                 }
             }
 
-            Section {
-                HStack {
-                    Spacer()
-
-                    Button("Add Income") {
-                        DataController().addBudget(
-                            name: name,
-                            income: Double(amount) ?? 0.0,
-                            expenses: expenses,
-                            context: managedObjContext)
-                            
-                            dismiss()
-                        
-
-              
-                       
-                    }
-                    .padding()
-
-                    Spacer()
-                }
+            Button("Add Income") {
+                DataController().addBudget(
+                    name: name,
+                    income: Double(amount) ?? 0.0,
+                    expenses: expenses,
+                    context: managedObjContext)
+                dismiss()
             }
+            .padding()
         }
     }
 }
