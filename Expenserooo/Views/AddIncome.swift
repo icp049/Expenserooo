0211933859b87f20f7518ddc1d54c6c819aa682c
icp049 <ianpepe2000@gmail.com>
2024-02-13 20:59:14 -0600

    prototyping AddIncome View

---
+++ Expenserooo/Views/AddIncome.swift
@@ -0,0 +1,20 @@
+//
+//  AddIncome.swift
+//  Expenserooo
+//
+//  Created by Ian Pedeglorio on 2024-02-13.
+//
+
+import SwiftUI
+
+struct AddIncome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddIncome_Previews: PreviewProvider {
+    static var previews: some View {
+        AddIncome()
+    }
+}
