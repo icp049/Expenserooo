5f927687fa960268609ea459100613181957a4c1
icp049 <ianpepe2000@gmail.com>
2024-02-20 23:53:44 -0600

    modified binding var

--- Expenserooo/Views/AddIncome.swift
+++ Expenserooo/Views/AddIncome.swift
@@ -1,40 +1,24 @@
+import Foundation
 import SwiftUI
-import CoreData
+
 
 struct AddIncomeView: View {
     @Environment(\.managedObjectContext) var managedObjContext
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var dataController: DataController // Access DataController as an environment object
-    
+
     @FetchRequest(
         entity: Income.entity(),
         sortDescriptors: [NSSortDescriptor(keyPath: \Income.date, ascending: false)],
         animation: .default
     )
     var incomes: FetchedResults<Income>
-    
+
     @State private var name = ""
     @State private var amount = ""
-    @State private var totalIncome: Double = 0.0 // State variable to hold the total income
-    
+    @Binding var totalIncome: Double // Binding for total income
+
     var body: some View {
-        
-        
-        
-        VStack{
-            
-            TextField("Total Income", value: $totalIncome, formatter: NumberFormatter())
-                .disabled(true)
-                .padding()
-            
-        }
-        .onAppear {
-            totalIncome = calculateTotalIncome(incomes)
-        }
-        
-        
-        
-        
         VStack {
             TextField("Income Name", text: $name)
                 .autocapitalization(.none)
@@ -78,10 +62,5 @@ struct AddIncomeView: View {
             dataController.save(context: managedObjContext)
         }
     }
-    
-    private func calculateTotalIncome(_ incomes: FetchedResults<Income>) -> Double {
-        return incomes.reduce(0) { $0 + ($1.amount ?? 0) }
-    }
-
 }
 
