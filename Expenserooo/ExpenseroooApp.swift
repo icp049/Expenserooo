33d584c282a2abaf7e3282bca0a6d2e73d390a28
icp049 <ianpepe2000@gmail.com>
2024-03-29 21:20:30 -0600

    modified

--- Expenserooo/ExpenseroooApp.swift
+++ Expenserooo/ExpenseroooApp.swift
@@ -6,29 +6,14 @@ struct ExpenseroooApp: App {
 
     var body: some Scene {
         WindowGroup {
-            TabView {
-         
-                
-                NavigationView {
-                    HomeView()
-                }
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
-                }
-                
-                
-                
+          
                 NavigationView {
                     BudgetListView()
                         .environment(\.managedObjectContext, dataController.container.viewContext)
                         .environmentObject(dataController) // Pass DataController as an environment object
                 }
-                .tabItem {
-                    Image(systemName: "list.bullet")
-                    Text("Budget List")
-                }
-            }
+             
+            
         }
     }
 }
